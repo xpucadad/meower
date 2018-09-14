958b9db420084001d4ea1c0d29654848a5d6ff16
Ken Reed <github@kmreed.net>
2018-09-13 21:11:28 -0700

    Checkpoint before server stuff starts

--- client/client.js
+++ client/client.js
@@ -1,89 +1,95 @@
 console.log('Hello World!');
 
 const form = document.querySelector('form'); // grabbing an element on the page
-const errorElement = document.querySelector('.error-message');
+
+// const errorElement = document.querySelector('.error-message');
 const loadingElement = document.querySelector('.loading');
-const mewsElement = document.querySelector('.mews');
-const API_URL = window.location.hostname === '127.0.0.1' ? 'http://localhost:5000/mews' : 'https://meower-api.now.sh/mews';
-
-errorElement.style.display = 'none';
-
-listAllMews();
-
+loadingElement.style.display='none';
+// const mewsElement = document.querySelector('.mews');
+// const API_URL = window.location.hostname === '127.0.0.1' ? 'http://localhost:5000/mews' : 'https://meower-api.now.sh/mews';
+//
+// errorElement.style.display = 'none';
+//
+// listAllMews();
+//
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = new FormData(form);
   const name = formData.get('name');
   const content = formData.get('content');
-
-  if (name.trim() && content.trim()) {
-    errorElement.style.display = 'none';
-    form.style.display = 'none';
-    loadingElement.style.display = '';
-
+//
+//   if (name.trim() && content.trim()) {
+//     errorElement.style.display = 'none';
+//     form.style.display = 'none';
+//     loadingElement.style.display = '';
+//
     const mew = {
       name,
       content
     };
-    
-    fetch(API_URL, {
-      method: 'POST',
-      body: JSON.stringify(mew),
-      headers: {
-        'content-type': 'application/json'
-      }
-    }).then(response => {      
-      if (!response.ok) {
-        const contentType = response.headers.get('content-type');
-        if (contentType.includes('json')) {
-          return response.json().then(error => Promise.reject(error.message));
-        } else {
-          return response.text().then(message => Promise.reject(message));
-        }
-      }
-    }).then(() => {
-      form.reset();
-      setTimeout(() => {
-        form.style.display = '';
-      }, 30000);
-      listAllMews();
-    }).catch(errorMessage => {
-      form.style.display = '';
-      errorElement.textContent = errorMessage;
-      errorElement.style.display = '';
-      loadingElement.style.display = 'none';
-    });
-  } else {
-    errorElement.textContent = 'Name and content are required!';
-    errorElement.style.display = '';
-  }
+
+    console.log(mew);
+    form.style.display='none';
+    loadingElement.style.display='';
+//
+//     fetch(API_URL, {
+//       method: 'POST',
+//       body: JSON.stringify(mew),
+//       headers: {
+//         'content-type': 'application/json'
+//       }
+//     }).then(response => {
+//       if (!response.ok) {
+//         const contentType = response.headers.get('content-type');
+//         if (contentType.includes('json')) {
+//           return response.json().then(error => Promise.reject(error.message));
+//         } else {
+//           return response.text().then(message => Promise.reject(message));
+//         }
+//       }
+//     }).then(() => {
+//       form.reset();
+//       setTimeout(() => {
+//         form.style.display = '';
+//       }, 30000);
+//       listAllMews();
+//     }).catch(errorMessage => {
+//       form.style.display = '';
+//       errorElement.textContent = errorMessage;
+//       errorElement.style.display = '';
+//       loadingElement.style.display = 'none';
+//     });
+//   } else {
+//     errorElement.textContent = 'Name and content are required!';
+//     errorElement.style.display = '';
+//   }
 });
 
 
-function listAllMews() {
-  mewsElement.innerHTML = '';
-  fetch(API_URL)
-    .then(response => response.json())
-    .then(mews => {
-      mews.reverse();
-      mews.forEach(mew => {
-        const div = document.createElement('div');
-
-        const header = document.createElement('h3');
-        header.textContent = mew.name;
-
-        const contents = document.createElement('p');
-        contents.textContent = mew.content;
-
-        const date = document.createElement('small');
-        date.textContent = new Date(mew.created);
-
-        div.appendChild(header);
-        div.appendChild(contents);
-        div.appendChild(date);
-
-        mewsElement.appendChild(div);
-      });
-      loadingElement.style.display = 'none';
-    });
-}
+// function listAllMews() {
+//   mewsElement.innerHTML = '';
+//   fetch(API_URL)
+//     .then(response => response.json())
+//     .then(mews => {
+//       mews.reverse();
+//       mews.forEach(mew => {
+//         const div = document.createElement('div');
+//
+//         const header = document.createElement('h3');
+//         header.textContent = mew.name;
+//
+//         const contents = document.createElement('p');
+//         contents.textContent = mew.content;
+//
+//         const date = document.createElement('small');
+//         date.textContent = new Date(mew.created);
+//
+//         div.appendChild(header);
+//         div.appendChild(contents);
+//         div.appendChild(date);
+//
+//         mewsElement.appendChild(div);
+//       });
+//       loadingElement.style.display = 'none';
+//     });
+// }
